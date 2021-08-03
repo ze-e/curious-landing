cc545173fb28e756b2b775ddd0bbf347c6fde7dc
zrr <zrexrodriguez@gmail.com>
2021-08-03 13:22:46 -0600

    feat:adding carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
 const carouselElement = document.querySelector('.home__carousel');
 const carouselItems = document.querySelectorAll('.home__carousel-img');
-console.log(carouselItems.length);
+let currentItem = 0;
 
   function scroll(node, left) {
     if(node){
@@ -11,12 +11,21 @@ console.log(carouselItems.length);
     }
   }
 
-  function handleClick(i) {
-    console.log(i);
+  function handleClick(i, elementName) {
+    const Id = i.id.split(elementName)[1];
 
     if (carouselElement) {
-      const scrollLeft = Math.floor(carouselElement.scrollWidth  * (i.id / carouselItems.length));
+      const scrollLeft = Math.floor(carouselElement.scrollWidth  * (Id / carouselItems.length));
       scroll(carouselElement, scrollLeft);
     }
   }
 
+  function changeSlide(changeBy) {
+    currentItem += changeBy;
+    if(currentItem > carouselItems.length) currentItem === 0;
+
+    if (carouselElement) {
+      const scrollLeft = Math.floor(carouselElement.scrollWidth  * (currentItem / carouselItems.length));
+      scroll(carouselElement, scrollLeft);
+    }
+  }
