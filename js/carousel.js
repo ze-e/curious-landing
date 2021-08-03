d8205c9ea184c63397c6a0621ee4157313641432
zrr <zrexrodriguez@gmail.com>
2021-08-03 14:59:02 -0600

    feat:adding carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,14 @@
+//don't load script until document loads so we can query elements
+window.onload = () =>{
+
+//query
 const carouselElement = document.querySelector('.home__carousel');
 const carouselItems = document.querySelectorAll('.home__carousel-img');
+
+//set var
 let currentItem = 0;
+
+autoChangeSlide();
 
   function scroll(node, left) {
     if(node){
@@ -11,7 +19,7 @@ let currentItem = 0;
     }
   }
 
-  function handleClick(i, elementName) {
+  function clickChangeSlide(i, elementName) {
     const Id = i.id.split(elementName)[1];
 
     if (carouselElement) {
@@ -22,10 +30,21 @@ let currentItem = 0;
 
   function changeSlide(changeBy) {
     currentItem += changeBy;
-    if(currentItem > carouselItems.length) currentItem === 0;
+    if(currentItem  > carouselItems.length - 1) currentItem = 0;
 
     if (carouselElement) {
       const scrollLeft = Math.floor(carouselElement.scrollWidth  * (currentItem / carouselItems.length));
       scroll(carouselElement, scrollLeft);
     }
   }
+
+  function autoChangeSlide() {
+    setInterval(changeSlide, 5000, 1);
+  }
+
+//attach events
+document.querySelector('.button_left').addEventListener('click', ()=>{changeSlide(-1)});
+document.querySelector('.button_right').addEventListener('click', ()=>{changeSlide(1)});
+
+}
+
